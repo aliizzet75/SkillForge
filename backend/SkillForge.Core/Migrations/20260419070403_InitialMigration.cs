f29a737986ac1cb83e4d370d0cdcadef8bf82345
aliizzet75 <aliizzet75@gmail.com>
2026-04-19 07:04:09 +0000

    feat: add initial EF Core migration

---
+++ backend/SkillForge.Core/Migrations/20260419070403_InitialMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillForge.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SocialProvider",
+                table: "Users",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SocialProviderId",
+                table: "Users",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SocialProvider",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SocialProviderId",
+                table: "Users");
+        }
+    }
+}
